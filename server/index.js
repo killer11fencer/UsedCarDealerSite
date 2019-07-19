2292c6e90290cf438ba8e677d549676e1d0e8104
Jeoffrey <jeoff.howe@gmail.com>
2019-07-18 23:20:00 -0600

    server up

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 express = require('express')
 session = require('express-session')
 massive = require('massive')
+const {SERVER_PORT,CONNECTION_STRING,SESSION_SECRET} = process.env
+
 
 app = express()
 
+massive(CONNECTION_STRING).then(db => {
+    app.set('db',db);
+    console.log(db.listTables())
+    console.log(`DATABASE: Connected`);
+    app.listen(SERVER_PORT,()=>console.log('listening on Port',SERVER_PORT))
+}).catch(err => console.log('err on db',err))
