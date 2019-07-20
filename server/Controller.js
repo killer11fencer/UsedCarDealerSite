81ce85ed6bf4f2b1612dfc8f422d41b4c18f2b72
Jeoffrey <jeoff.howe@gmail.com>
2019-07-19 23:26:12 -0600

    adding controller

--- server/Controller.js
+++ server/Controller.js
@@ -2,6 +2,12 @@ module.exports = {
     getMake: (req,res) => {
         const db = req.app.get('db');
         db.getMake().then(result => res.status(200).send(result))
-        .catch(err => console.log('err on make'))
-    }
+        .catch(err => console.log('err on make',err))
+    },
+    getModel: (req,res) => {
+        const make_id = req.params;
+        const db = req.app.get('db',db);
+        db.getModel({make_id}).then(result => res.status(200).send(result))
+        .catch(err => console.log('err on models',err))
+    },
 }
