e0ff6a4810f9646fa5b604335ed60885584af959
Jeoffrey <jeoff.howe@gmail.com>
2019-07-19 23:44:40 -0600

    fixed SQL query

--- db/getNewListing.sql
+++ db/getNewListing.sql
@@ -2,4 +2,5 @@ select type,drive,price,miles,description, status, model_name, year, make from l
 join car_model on car_model.id = listing.model_id
 join car_make on car_make.id = listing.car_id
 join car_type on car_type.id = listing.model_id
-where model_id = ${model_id};
+order by listing.id desc
+limit 4;
