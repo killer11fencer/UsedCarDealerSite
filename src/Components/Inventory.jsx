cb5fee21db874e497415a0fd72ed33becbff69d5
Jeoffrey <jeoff.howe@gmail.com>
2019-07-23 23:59:02 -0600

    added new component

---
+++ src/Components/Inventory.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class Inventory extends Component {
+    constructor () {
+        super();
+        this.state = {
+            inventory: []
+        }
+    
+    }
+    componentDidMount () {
+        this.getInventory()
+    }
+    getInventory = () => {
+        axios.get('/api/inventroy').then(res=> this.setState({inventory:res.data}))
+        .catch(err => console.log('err on inventory',err))
+    }
+}
