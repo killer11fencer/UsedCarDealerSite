8dc332225ee7b55a543ece8f1c754feff5e0a4fb
Jeoffrey <jeoff.howe@gmail.com>
2019-07-26 23:44:07 -0600

    working on register

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -4,6 +4,13 @@ import axios from 'axios'
 class Register extends Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            confirm_password:''
+        }
     }
+    
 }
