1eef448506e87def9b4a7f9d09c187e91957f637
Jeoffrey <jeoff.howe@gmail.com>
2019-07-27 19:48:32 -0600

    adding inputs

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -21,8 +21,21 @@ handleSubmit = () => {
     
     if(this.state.password === this.state.confirm_password && this.state.password.length > 1) {
         const {first_name,last_name,email,password,confirm_password} = this.state
-        axios.post('/api/register',{first_name,last_name,email,password,confirm_password})
+        axios.post('/api/register',{first_name,last_name,email,password,confirm_password}).then(()=>this.setState)
     }
 }
-
+render() {
+    return(
+        <div>
+            <h5>First Name</h5>
+            <input name='first_name' onChange={this.handleChange} value={this.state.first_name}/>
+            <h5>Last Name</h5>
+            <h5>Email</h5>
+            <h5>Password</h5>
+            <h5>Confirm Password</h5>
+            <button>Submit</button>
+            <button>Cancel</button>
+        </div>
+    )
+}
 }
