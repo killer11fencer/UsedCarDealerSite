1272d6673e20ee6b74d1e92732bdd4e04188fa85
Jeoffrey <jeoff.howe@gmail.com>
2019-07-28 22:34:16 -0600

    added about page

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About () {
+    render () {
+        return (
+            <div>
+                <h1>Hours</h1>
+                <h1>Location</h1>
+            </div>
+        )
+    }
+}
