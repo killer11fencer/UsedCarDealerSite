1bf06b981f8b8abaa1f3cc8b18f5e533bdc630d2
Jeoffrey <jeoff.howe@gmail.com>
2019-08-03 23:20:37 -0600

    contact page added

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            referrals: []
+        }
+    }
+}
