3b42e1e19232d0ed0ed805eb5a8fedd3d121c4da
Jeoffrey <jeoff.howe@gmail.com>
2019-08-19 00:18:32 -0600

    added function

--- src/Components/notes.jsx
+++ src/Components/notes.jsx
@@ -6,3 +6,28 @@ RENDER PROPS
 let volleyball = () => {
     return 'hello'
 }
+
+let test = [ 5,'ab','aab','abb','abab','abaaaba']
+
+function minimalOperations(words) {
+  let wordArr = words.filter((item)=>{
+    return !(parseInt(item) == item);
+  })
+    let numArr = wordArr.map(word => {
+      let letters = word.split('')
+     let filter = letters.map((letter,i) => {
+       console.log(letters[i-1])
+       if(letters[i-1] === letter) {
+         return false
+       }
+      else {return true}
+     })
+     if(filter.includes(false)) {
+       return 1
+     } else {return 0}
+    
+    })
+    return numArr
+
+     
+}
